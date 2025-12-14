b3ab639a41d09dff062e9c85f9d71764cf636dea
Julien Capellari <julien.capellari@gmail.com>
2025-12-15 00:00:38 +0100

    Setup GitHub app auth (#2)
    * feat: login to github as app installation
    
    * chore: update graphql config
    
    * chore: regenerate graphql types
    
    * fix: lint errors
    
    * chore: ignore missing dotenv file
    
    * feat: prevent token leaking to frontend
    
    * chore: self review

---
+++ lib/github.auth.ts
@@ -0,0 +1,13 @@
+import { createAppAuth, type InstallationAccessTokenAuthentication } from '@octokit/auth-app';
+
+const appAuth = createAppAuth({
+  appId: process.env.GITHUB_APP_ID!,
+  installationId: process.env.GITHUB_INSTALLATION_ID,
+  clientId: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  privateKey: process.env.GITHUB_PRIVATE_KEY!,
+});
+
+export async function githubAppToken(): Promise<InstallationAccessTokenAuthentication> {
+  return await appAuth({ type: 'installation' });
+}
