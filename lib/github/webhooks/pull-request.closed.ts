b8fa891c1f2a179cd4383567d926a610750066be
Julien Capellari <julien.capellari@gmail.com>
2026-08-07 00:47:01 +0200

    fix: pr closed webhook

--- lib/github/webhooks/pull-request.closed.ts
+++ lib/github/webhooks/pull-request.closed.ts
@@ -10,34 +10,44 @@ export async function pullRequestClosedHook({
   const { owner, name } = splitRepositoryFullName(repository.full_name);
   const pushedAt = repository.pushed_at ? dayjs(repository.pushed_at).toISOString() : null;
 
-  await Promise.all([
-    prisma.repository.update({
-      where: {
-        fullName: {
-          owner,
-          name,
+  await prisma.repository.update({
+    where: {
+      fullName: {
+        owner,
+        name,
+      },
+    },
+    data: {
+      issueCount: repository.open_issues_count,
+      pullRequestCount: { decrement: 1 },
+      pushedAt,
+      pullRequests: {
+        upsert: {
+          where: {
+            fullNumber: {
+              repositoryOwner: owner,
+              repositoryName: name,
+              number: pull_request.number,
+            },
+          },
+          update: {
+            title: pull_request.title,
+            state: pull_request.state,
+            author: pull_request.user.login,
+            updatedAt: dayjs(pull_request.updated_at).toDate(),
+          },
+          create: {
+            number: pull_request.number,
+            title: pull_request.title,
+            state: pull_request.state,
+            author: pull_request.user.login,
+            createdAt: dayjs(pull_request.created_at).toDate(),
+            updatedAt: dayjs(pull_request.updated_at).toDate(),
+          },
         },
       },
-      data: {
-        issueCount: repository.open_issues_count,
-        pullRequestCount: { decrement: 1 },
-        pushedAt,
-      },
-    }),
-    prisma.pullRequest.update({
-      where: {
-        fullNumber: {
-          repositoryOwner: owner,
-          repositoryName: name,
-          number: pull_request.number,
-        },
-      },
-      data: {
-        state: pull_request.state,
-        updatedAt: dayjs(pull_request.updated_at).toDate(),
-      },
-    }),
-  ]);
+    },
+  });
 
   revalidateTag('repositories', 'max');
 }
