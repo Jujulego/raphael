21b6663f22b6b0e0a3fc00535eb25949aaa2b9e1
Julien Capellari <julien.capellari@gmail.com>
2026-09-03 00:33:37 +0200

    feat: setup dynamic base url

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -4,6 +4,10 @@ import { betterAuth } from 'better-auth';
 import { nextCookies } from 'better-auth/next-js';
 
 export const auth = betterAuth({
+  baseURL: {
+    allowedHosts: ['localhost:3000', 'raphael-*-jujulego.vercel.app', 'raphael-iota.vercel.app'],
+    protocol: process.env.NODE_ENV === 'development' ? 'http' : 'https',
+  },
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
